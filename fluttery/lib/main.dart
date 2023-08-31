a65973ce94e097b9459aa19c68ef7429baa12fa7
Lugonvu Mayimuna Kizza <munalugonvu@gmail.com>
2023-08-31 23:45:27 +0300

    update file uto use the input widget

--- fluttery/lib/main.dart
+++ fluttery/lib/main.dart
@@ -1,32 +1,20 @@
 import 'package:flutter/material.dart';
-import 'catalogue/car/car_info_widget.dart'; // Import the CarInfoWidget class
+import 'checkInput/input.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final List<Car> cars = [
-    Car(numberPlate: 'ABC123', color: 'Blue', engineNumber: 'E12345'),
-    Car(numberPlate: 'DEF456', color: 'Red', engineNumber: 'E67890'),
-  ];
+ 
 
-   MyApp({super.key});
+   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Car Information'),
-        ),
-        body: ListView.builder(
-          itemCount: cars.length,
-          itemBuilder: (context, index) {
-            return CarInfoWidget(car: cars[index]);
-          },
-        ),
-      ),
+    return const MaterialApp(
+      home: InputWidget(),
+     
     );
   }
 }
